0d927734d9c048c6ba825cacd08b6b11c8372161
Nathan Campbell <nathancampbell@outlook.com>
2017-10-05 20:31:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ function getRepositories() {
 function displayRepositories() {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = 
+  const repoList = "<ul>" + repos.map(repo => {
+   const dataUsername = 'data-username="' + repo.owner.login + '"'
+   const dataRepoName = 'data-repository="' + repo.name + '"'
+   return (`
+     <li>
+     <h2> ${repo.name} </h2>
+     <a href="${repo.html_url}">${repo.html_url}</a><br>
+     <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+     <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a>
+     </li>
+   `) }).join("") + "</ul>"
+   document.getElementById("repositories").innerHTML = repoList
 }
 
 function getCommits(el) {
