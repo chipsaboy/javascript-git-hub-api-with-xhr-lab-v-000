d29210d369da0d98d5535d3c85bce818e78f7021
Nathan Campbell <nathancampbell@outlook.com>
2017-10-05 20:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ function getRepositories() {
   req.open("GET", link)
   req.send()
 }
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = 
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/chipsaboy/' + name + '/commits')
+  req.send()
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.author.name + ' (' +  commit.author.login + ')</strong>' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
